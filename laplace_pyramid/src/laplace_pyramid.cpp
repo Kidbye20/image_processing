55450255dd24f0fabb5aa037ad022dcf7a429a3c
hermosayhl <2535894692@qq.com>
2022-01-06 01:42:50 +0800

    拉普拉斯金字塔分解完毕! 开始压缩信息

--- laplace_pyramid/src/laplace_pyramid.cpp
+++ laplace_pyramid/src/laplace_pyramid.cpp
@@ -33,10 +33,26 @@ namespace {
         std::cout << "是否连续" << std::boolalpha << one_image.isContinuous() << std::endl;
     }
 
+
+    cv::Mat cv_concat(const std::vector<cv::Mat> images, const bool v=false) {
+        cv::Mat result;
+        if(not v) cv::hconcat(images, result);
+        else cv::vconcat(images, result);
+        return result;
+    }
+
     cv::Mat make_pad(const cv::Mat& one_image, const int pad_H, const int pad_W) {
         cv::Mat padded_image;
         cv::copyMakeBorder(one_image, padded_image, pad_H, pad_H, pad_W, pad_W, cv::BORDER_REPLICATE);
         return padded_image;
+    }
+
+    template<typename T>
+    cv::Mat toint8(const std::vector<T>& source, const int H, const int W, const int C, const int _type) {
+        cv::Mat result(H, W, _type);
+        const int length = H * W * C;
+        for(int i = 0;i < length; ++i) result.data[i] = std::abs(source[i]);
+        return result;
     }
 }
 
@@ -150,22 +166,81 @@ cv::Mat pyramid_upsample(const cv::Mat& source) {
     return upsampled;
 }
 
+void pyramid_upsample_interpolate(const cv::Mat& source) {
+    const int H = source.rows, W = source.cols;
+    const int C = source.channels();
+    // 先处理第一行
+    for(int ch = 0; ch < C; ++ch) {
+        for(int j = 1;j < W - 1; j += 2)
+            source.data[j * C + ch] = (source.data[(j - 1) * C + ch] + source.data[(j + 1) * C + ch]) / 2;
+        source.data[(W - 1) * C + ch] = source.data[(W - 2) * C + ch];
+    }
+    // 最后一行
+    uchar* const row_ptr = source.data + (H - 1) * W * C;
+    for(int ch = 0; ch < C; ++ch) {
+        for(int j = 1;j < W - 1; j += 2)
+            row_ptr[j * C + ch] = (row_ptr[(j - 1) * C + ch] + row_ptr[(j + 1) * C + ch]) / 2;
+        row_ptr[(W - 1) * C + ch] = row_ptr[(W - 2) * C + ch];
+    }
+    // 第一列
+    for(int ch = 0; ch < C; ++ch) {
+        for(int i = 1;i < H - 1; i += 2) {
+            const int pos = i * W * C + ch;
+            source.data[pos] = (source.data[pos - W * C] + source.data[pos + W * C]) / 2;
+        }
+    }
+    // 剩下的行和列
+    for(int ch = 0; ch < C; ++ch) {
+        for(int i = 1;i < H - 1; ++i) {
+            uchar* const row_ptr = source.data + i * W * C;
+            for(int j = 1;j < W - 1; ++j) {
+                // 如果都是奇数, 说明是空的
+                if((i & 1) and (j & 1)) {
+                    row_ptr[j * C + ch] = (row_ptr[(j - 1 - W) * C + ch] + row_ptr[(j - 1 + W) * C + ch] + row_ptr[(j + 1 - W) * C + ch] + row_ptr[(j + 1 + W) * C + ch]) / 4;
+                }
+                // 如果奇数行, 偶数列
+                else if(i & 1) {
+                    row_ptr[j * C + ch] = (row_ptr[(j - W) * C + ch] + row_ptr[(j + W) * C + ch]) / 2;
+                }
+                // 如果偶数行, 奇数列
+                else if(j & 1) {
+                    row_ptr[j * C + ch] = (row_ptr[(j - 1) * C + ch] + row_ptr[(j + 1) * C + ch]) / 2;
+                }
+            }
+        }
+    }
+    // 最后一列是全黑的 ! 因为之前 upsample 的时候偶数列全部都是 0, 直接把倒数第二列拷贝过去
+    // 而倒数第二列在前面的大循环中才会赋值, 所以放到这里
+    for(int ch = 0; ch < C; ++ch) {
+        for(int i = 1;i < H - 1; ++i) {
+            const int pos = (i * W + W - 1) * C + ch;
+            source.data[pos] = source.data[pos - C];
+        }
+    }
+}
 
 
-std::vector<cv::Mat> build_laplace_pyramid(const std::vector<cv::Mat>& gaussi_pyramid) {
+
+std::vector< std::vector<int> > build_laplace_pyramid(const std::vector<cv::Mat>& gaussi_pyramid) {
     // 查看几层
     const int layers_num = gaussi_pyramid.size();
     // 准备一个结果
-    std::vector<cv::Mat> laplace_pyramid;
+    std::vector< std::vector<int> > laplace_pyramid;
     laplace_pyramid.reserve(layers_num - 1);
     // 从低分辨率开始构建拉普拉斯金字塔
     for(int i = layers_num - 1; i >= 1; --i) {
         // 首先低分辨率先上采样到两倍大小
         cv::Mat upsampled = pyramid_upsample(gaussi_pyramid[i]);
-        // 使用 4 倍的高斯滤波, 这个得格外写
-        upsampled = fast_gaussi_blur(upsampled, 2, 1.0, 4.0, true);
-        cv_show(upsampled);
+        // 填补值
+        pyramid_upsample_interpolate(upsampled);
+        // 放到拉普拉斯金字塔
+        const int length = upsampled.rows * upsampled.cols * upsampled.channels();
+        std::vector<int> residual(length, 0);
+        for(int k = 0;k < length; ++k)
+            residual[k] = gaussi_pyramid[i - 1].data[k] - upsampled.data[k];
+        laplace_pyramid.emplace_back(residual);
     }
+    std::reverse(laplace_pyramid.begin(), laplace_pyramid.end());
     return laplace_pyramid;
 }
 
@@ -176,12 +251,61 @@ void laplace_decomposition_demo() {
     const std::string save_dir("./images/output/");
     cv::Mat origin_image = cv::imread(image_path);
     assert(!origin_image.empty() and "图片读取失败");
+    // 构建层数
+    const int layers_num = 5;
     // 根据图像构建高斯金字塔
-    const auto gaussi_pyramid = build_gaussi_pyramid(origin_image, 5);
+    const auto gaussi_pyramid = build_gaussi_pyramid(origin_image, layers_num);
     // 构建拉普拉斯金字塔
     const auto laplace_pyramid = build_laplace_pyramid(gaussi_pyramid);
+    // 展示
+    cv::Mat reconstructed = gaussi_pyramid[layers_num - 1];
+    for(int i = layers_num - 2;i >= 0; --i) {
+        cv::Mat upsampled = pyramid_upsample(reconstructed);
+        pyramid_upsample_interpolate(upsampled);
+        // 将 laplace_pyramid 和 当前结果结合
+        const int H = upsampled.rows, W = upsampled.cols, C = upsampled.channels();
+        const int length = H * W * C;
+        std::vector<uchar> temp(length, 0);
+        for(int k = 0;k < length; ++k)
+            temp[k] = cv::saturate_cast<uchar>(upsampled.data[k] + laplace_pyramid[i][k]);
+        // 再把结果拷贝到 reconstructed
+        reconstructed = cv::Mat(H, W, upsampled.type()); // reconstructed 的大小得变化一下
+        std::memcpy(reconstructed.data, temp.data(), length);
+        // 观察损失有多大
+        std::cout << "PSNR ===>  " << cv::PSNR(reconstructed, gaussi_pyramid[i]) << "db" << std::endl;
+        cv_show(cv_concat({
+            gaussi_pyramid[i],
+            upsampled,
+            toint8(laplace_pyramid[i], H, W, C, upsampled.type()),
+            reconstructed}));
+    }
 }
 
+
+// 从上面可以看出, 如何压缩信息 ?
+// 最小分辨率的图像 + 拉普拉斯金字塔
+// 而拉普拉斯金字塔很多都是 0, 所以只需要保留那些大于某个阈值的点的坐标信息, 即可大概还原出图像来
+cv::Mat laplace_pyramid_reconstruct(
+        const cv::Mat& low_res,
+        const std::vector< std::vector<int> >& laplace_pyramid,
+        const int layers_num) {
+    // 从最小分辨图像开始
+    cv::Mat reconstructed = low_res.clone();
+    for(int i = layers_num - 2;i >= 0; --i) {
+        cv::Mat upsampled = pyramid_upsample(reconstructed);
+        pyramid_upsample_interpolate(upsampled);
+        // 将 laplace_pyramid 和 当前结果结合
+        const int H = upsampled.rows, W = upsampled.cols, C = upsampled.channels();
+        const int length = H * W * C;
+        std::vector<uchar> temp(length, 0);
+        for(int k = 0;k < length; ++k)
+            temp[k] = cv::saturate_cast<uchar>(upsampled.data[k] + laplace_pyramid[i][k]);
+        // 再把结果拷贝到 reconstructed
+        reconstructed = cv::Mat(H, W, upsampled.type()); // reconstructed 的大小得变化一下
+        std::memcpy(reconstructed.data, temp.data(), length);
+    }
+    return reconstructed;
+}
 
 
 int main() {
